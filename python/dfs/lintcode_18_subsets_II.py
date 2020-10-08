499719aabecec53572cb07e7d024ddb4dd42b41c
wodeleeway <weizi185@gmail.com>
2020-10-08 15:17:37 -0400

    lintcode - 18 - subsets II

---
+++ python/dfs/lintcode_18_subsets_II.py
@@ -0,0 +1,63 @@
+"""
+author: Wei Li
+date: 10/08/2020
+
+https://www.lintcode.com/problem/subsets-ii/description?_from=ladder&&fromId=161
+
+18. Subsets
+
+Given a collection of integers that might contain duplicates, nums, return all possible subsets (the power set).
+
+样例
+Example 1:
+
+Input: [0]
+Output:
+[
+  [],
+  [0]
+]
+Example 2:
+
+Input: [1,2,2]
+Output:
+[
+  [2],
+  [1],
+  [1,2,2],
+  [2,2],
+  [1,2],
+  []
+]
+Challange:
+Can you do it in both recursively and iteratively?
+
+Constraint
+Each element in a subset must be in non-descending order.
+The ordering between two subsets is free.
+The solution set must not contain duplicate subsets.
+"""
+class Solution:
+    """
+    @param nums: A set of numbers.
+    @return: A list of lists. All valid subsets.
+    """
+    def subsetsWithDup(self, nums):
+        # write your code here
+        nums.sort()
+        
+        ans = []
+        self.helper(nums, 0, [], ans)
+        
+        return ans
+    
+    def helper(self, nums, index, formed, ans):
+        ans.append(formed[:])
+        
+        for i in range(index, len(nums)):
+            if i > index and nums[i] == nums[i - 1]:
+                continue
+            
+            formed.append(nums[i])
+            self.helper(nums, i + 1, formed, ans)
+            formed.pop()
