17250567022b490bf5c0b90e8dddc357e56b9925
Zhengjian Kang <zk299@nyu.edu>
2020-10-17 19:40:59 -0400

    minor change for leetcode 437

--- python/tree_bst/leetcode_437_path_sum_iii_medium_frq40.py
+++ python/tree_bst/leetcode_437_path_sum_iii_medium_frq40.py
@@ -31,35 +31,37 @@ Return 3. The paths that sum to 8 are:
 1.  5 -> 3
 2.  5 -> 2 -> 1
 3. -3 -> 11
-""" 
+"""
 # Recursion
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
 class Solution:
-    def pathSum(self, root: TreeNode, target: int) -> int:
+    def pathSum(self, root: TreeNode, sum: int) -> int:
         if not root:
             return 0
-        
         hash_map = collections.defaultdict(int)
-        
-        return self.dfs(root, 0, target, hash_map)
-    
-    
+        hash_map[0] = 1
+        return self.dfs(root, 0, sum, hash_map)
+
     def dfs(self, root, curr_sum, target, hash_map):
         count = 0
         if not root:
             return 0
-        
+
         curr_sum += root.val
-                
-        if curr_sum == target:
-            count += 1
-            
         count += hash_map[curr_sum - target]
-        
+
         hash_map[curr_sum] += 1
-        
+
         count += self.dfs(root.left, curr_sum, target, hash_map)
         count += self.dfs(root.right, curr_sum, target, hash_map)
-        
+
         hash_map[curr_sum] -= 1
-        
+
         return count
