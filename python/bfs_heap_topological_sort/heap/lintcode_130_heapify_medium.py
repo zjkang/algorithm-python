ea30bdbbb9ff9f8ccd562a7f17530d6002bd454e
Zhengjian Kang <zk299@nyu.edu>
2020-10-22 23:36:42 -0400

    lintcode 1272 kth smallest element in a sorted matrix

--- python/bfs_heap_topological_sort/heap/lintcode_130_heapify_medium.py
+++ python/bfs_heap_topological_sort/heap/lintcode_130_heapify_medium.py
@@ -7,7 +7,9 @@ https://www.lintcode.com/problem/heapify/description
 130. Heapify
 
 Given an integer array, heapify it into a min-heap array.
-For a heap array A, A[0] is the root of heap, and for each A[i], A[i * 2 + 1] is the left child of A[i] and A[i * 2 + 2] is the right child of A[i].
+For a heap array A, A[0] is the root of heap, and for each A[i], 
+A[i * 2 + 1] is the left child of A[i] and A[i * 2 + 2] is the right child 
+of A[i].
 
 Example
 Example 1
@@ -21,8 +23,10 @@ O(n) time complexity
 
 
 # Heapify 的具体实现方法。时间复杂度为 O(n)O(n)，使用的是 siftdown
-# 之所以是 O(n) 是因为从第 N/2 个位置开始往下 siftdown，那么就有大约 N/4 个数在 siftdown 中最多交换 1 次，N/8 个数最多交换 2 次，N/16 个数最多交换 3 次。
-# 所以 O(N/4 * 1 + N/8 * 2 + N/16 * 3 + ... + 1 * LogN) = O(N)O(N/4∗1+N/8∗2+N/16∗3+...+1∗LogN)=O(N)
+# 之所以是 O(n) 是因为从第 N/2 个位置开始往下 siftdown，那么就有大约 N/4
+# 个数在 siftdown 中最多交换 1 次，N/8 个数最多交换 2 次，N/16 个数最多交换 3 次。
+# 所以 O(N/4 * 1 + N/8 * 2 + N/16 * 3 + ... + 1 * LogN) =
+# O(N)O(N/4∗1+N/8∗2+N/16∗3+...+1∗LogN)=O(N)
 class Solution:
     """
     @param: A: Given an integer array
