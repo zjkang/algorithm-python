80a9c3f81f5b0a9fc98dfcc5831961e52bf1a0c9
Zhengjian Kang <zk299@nyu.edu>
2020-11-14 20:46:58 -0500

    leetcode 523 continuous subarray sum

--- python/pre_sum/lintcode_138_subarray_sum.py
+++ python/pre_sum/lintcode_138_subarray_sum.py
@@ -17,28 +17,29 @@ Explanation: return anyone that the sum is 0.
 Example 2:
 
 Input:  [-3, 1, -4, 2, -3, 4]
-Output: [1,5]	
+Output: [1,5]
 注意事项
 There is at least one subarray that it's sum equals to zero.
 """
+
+
 class Solution:
     """
     @param nums: A list of integers
     @return: A list of integers includes the index of the first number and the index of the last number
     """
+
     def subarraySum(self, nums):
         # write your code here
         pre_sum_mapping = {0: 0}
-        
+
         pre_sum = 0
         for i, num in enumerate(nums):
             pre_sum += num
-            
+
             if pre_sum in pre_sum_mapping:
                 return [pre_sum_mapping[pre_sum], i]
-                
-        
+
             pre_sum_mapping[pre_sum] = i + 1
-        
-        return [-1, -1]    
-        
+
+        return [-1, -1]
