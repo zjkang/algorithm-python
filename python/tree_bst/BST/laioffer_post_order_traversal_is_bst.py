cdbf9d4a7acba6f30d7e0b76fbadc2969ef1706b
Zhengjian Kang <zk299@nyu.edu>
2020-12-10 21:19:05 -0500

    Create laioffer_post_order_traversal_is_bst.py

---
+++ python/tree_bst/BST/laioffer_post_order_traversal_is_bst.py
@@ -0,0 +1,27 @@
+'''
+Given a sequence of post-order traversal, determine whether the binary tree is binary search tree or not
+         5
+       3.   8
+     1.  4 6  10
+     
+post order: 1,4,3,6,10,8,5
+'''
+
+
+def is_bst(nums, start, end):
+    if start >= end:
+        return True
+    cur = start
+    while cur < end and nums[cur] < nums[end]:
+        cur += 1
+    left_end = cur - 1
+    while cur < end:
+        if nums[cur] < nums[end]:
+            return False
+        cur += 1
+    return is_bst(nums, start, left_end) and is_bst(nums, left_end+1, end-1)
+
+
+# test
+nums = [1, 4, 3, 6, 10, 8, 5]
+assert(is_bst(nums, 0, len(nums)-1) is True)
