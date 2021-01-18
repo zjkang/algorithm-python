c3bd2982fdd400951cf91e91d6677f2b5530f396
Zhengjian Kang <zk299@nyu.edu>
2021-01-17 22:56:28 -0500

    update readme

--- python/union_find/leetcode_1697_checking_existence_of_edge_length_limited_paths.py
+++ python/union_find/leetcode_1697_checking_existence_of_edge_length_limited_paths.py
@@ -4,6 +4,8 @@ date: 01/17/2021
 
 https://leetcode.com/problems/checking-existence-of-edge-length-limited-paths/
 
+1697. Checking Existence of Edge Length Limited Paths
+
 An undirected graph of n nodes is defined by edgeList, where edgeList[i] =
 [ui, vi, disi] denotes an edge between nodes ui and vi with distance disi.
 Note that there may be multiple edges between two nodes.
--- python/union_find/leetcode_1724_checking_existence_of_edge_length_limited_paths_ii.py
+++ python/union_find/leetcode_1724_checking_existence_of_edge_length_limited_paths_ii.py
@@ -4,7 +4,9 @@ date: 01/17/2021
 
 https://leetcode.com/problems/checking-existence-of-edge-length-limited-paths-ii/
 
-n undirected graph of n nodes is defined by edgeList, where edgeList[i] =
+1724. Checking Existence of Edge Length Limited Paths II
+
+An undirected graph of n nodes is defined by edgeList, where edgeList[i] =
 [ui, vi, disi] denotes an edge between nodes ui and vi with distance disi.
 Note that there may be multiple edges between two nodes, and the graph may
 not be connected.
