ae154f7a97ae7afb008b6f216cb4f21ad31df04c
Zhengjian Kang <zk299@nyu.edu>
2021-02-26 17:29:55 -0500

    leetcode 1392

--- python/CANKU.md
+++ python/CANKU.md
@@ -17,7 +17,7 @@
 [02/10/2021 Leetcode 1754 Largest Merge Of Two Strings (M) String](https://github.com/zjkang/ds_algorithm/blob/main/python/array_string/leetcode_1754_largest_merge_of_two_strings.py)\
 [02/09/2021 Leetcode 1751 Maximum Number of Events That Can Be Attended II (H) Interval + DP](https://github.com/zjkang/ds_algorithm/blob/main/python/dp/leetcode_1751_maximum_number_of_events_that_can_be_attended_ii.py)\
 [02/08/2021 Leetcode 1749 Maximum Absolute Sum of Any Subarray (M) PreSum + DP](https://github.com/zjkang/ds_algorithm/blob/main/python/pre_sum/leetcode_1749_maximum_absolute_sum_of_any_subarray.py)\
-[02/01/2021 Leetcode 1745 Palindrome Partitioning IV (H) DP](https://github.com/zjkang/ds_algorithm/blob/main/python/dp/leetcode_1745_palindrome_partitioning_iv.py)\
+[02/01/2021 Leetcode 1745 Palindrome Partitioning IV (H) DP](https://github.com/zjkang/ds_algorithm/blob/main/python/dp/leetcode_1745_palindrome_partitioning_iv.py)
 
 ---
 
---
+++ python/array_string/leetcode_1392_longest_happy_prefix.py
@@ -0,0 +1,55 @@
+
+"""
+author: Zhengjian Kang
+date: 02/26/2021
+
+残酷群每日一题: 02/02/2021
+
+note: 打卡kmp的题目。这道题是自相关的match问题
+
+A string is called a happy prefix if is a non-empty prefix which is also a
+suffix (excluding itself).
+
+Given a string s. Return the longest happy prefix of s .
+
+Return an empty string if no such prefix exists.
+
+Example 1:
+Input: s = "level"
+Output: "l"
+Explanation: s contains 4 prefix excluding itself ("l", "le", "lev", "leve"),
+and suffix ("l", "el", "vel", "evel"). The largest prefix which is also
+suffix is given by "l".
+
+Example 2:
+Input: s = "ababab"
+Output: "abab"
+Explanation: "abab" is the largest prefix which is also suffix. They can
+overlap in the original string.
+
+Example 3:
+Input: s = "leetcodeleet"
+Output: "leet"
+
+Example 4:
+Input: s = "a"
+Output: ""
+
+Constraints:
+1 <= s.length <= 10^5
+s contains only lowercase English letters.
+"""
+
+
+class Solution:
+    def longestPrefix(self, s: str) -> str:
+        # refer huifeng suffix array
+        # https://github.com/wisdompeak/LeetCode/blob/master/String/1392.Longest-Happy-Prefix/1392.Longest-Happy-Prefix.cpp
+        n = len(s)
+        dp = [0] * n
+        for i in range(1, n):
+            j = dp[i-1]
+            while j >= 1 and s[i] != s[j]:
+                j = dp[j-1]
+            dp[i] = j + (s[j] == s[i])
+        return s[:dp[-1]]
