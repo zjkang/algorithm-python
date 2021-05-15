eeab4cea7ddb54c51f098a97a140d94727370f74
Zhengjian Kang <zk299@nyu.edu>
2021-05-15 18:11:45 -0400

    Update Template_Judge_Circle.py

--- python/graph/Template_Judge_Circle.py
+++ python/graph/Template_Judge_Circle.py
@@ -11,72 +11,45 @@ def main():
       return False
     return True
   
-
-  
-{
-  // prepare next; // next[i][j]: there is a directional path i->j
-
-  // dfs 判断有环
-  for (int i=0; i<numNodes; i++)
-  {            
-      if (dfs(i)==false) return false;            
-  }
-  return true;
-
-  // bfs 判断有环
-  return bfs();
-}
-
-bool dfs(int cur)
-{
-    if (visited[cur]==1) return true;
-
-    visited[cur] = 2;
-    for (int next: next[cur])
-    {
-        if (visited[next]==1) continue;
-        if (visited[next]==2) return false;
-        if (dfs(next)==false)  return false;
-    }
-    visited[cur] = 1;
-    return true;
-}
+  # bfs 判断有环
+  return bfs()
 
 
+def dfs(cur):
+  #1: visited, 2: visiting
+  if (visited[cur]==1):
+    return True
 
-bool bfs() 
-{        
-    queue<int>q;
-    int count = 0;
-
-    vector<int>InDegree(numNodes,0);
-    for (int i=0; i<numNodes; i++)
-    	for (int j: next[i])
-    		InDegree[j]++;
-
-    for (int i=0; i<numNodes; i++)
-    {
-        if (InDegree[i]==0) 
-        {
-            q.push(i);
-            count++;
-        }
-    }
-                
-    while (!q.empty())
-    {
-        int curCourse = q.front();
-        q.pop();
-        for (auto child: next[curCourse])
-        {
-            InDegree[child]--;
-            if (InDegree[child]==0)
-            {
-                q.push(child);
-                count++;
-            }                    
-        }
-    }        
+    visited[cur] = 2
+    for (next: next[cur]):
+        if (visited[next]==1): continue
+        if (visited[next]==2): return False
+        if (dfs(next)==False):  return False
     
-    return count==numNodes;        
-}
+    visited[cur] = 1
+    return True
+
+
+def bfs():
+  q = deque([])
+  count = 0
+  
+  InDegree = [0] * numNodes
+  for i in range(numNodes):
+    for j in next[i]:
+      InDegree[j] += 1
+  
+  for i in range(numNodes):
+    if InDegree[i] == 0:
+      q.append(i)
+      count += 1
+      
+  while q:
+    curCourse = q.popleft()
+    for child in next[curCourse]:
+      InDegree[child] -= 1
+      if InDegree[child] == 0:
+        q.append(child)
+        count += 1
+  
+  return count == numNodes
