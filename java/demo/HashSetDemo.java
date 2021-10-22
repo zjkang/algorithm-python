075c0a69f4b9fde084c22cde17aed4c87d1aceb4
Zhengjian Kang <zk299@nyu.edu>
2021-10-21 23:28:42 -0400

    Update HashSetDemo.java

--- java/demo/HashSetDemo.java
+++ java/demo/HashSetDemo.java
@@ -3,33 +3,33 @@ import java.io.*;
 import java.util.*;
 
 public class HashSetDemo {
-	public static void main(String args[])
-	{
-		// Creating an empty Set
-		Set<String> set = new HashSet<String>();
+    public static void main(String args[])
+    {
+        // Creating an empty Set
+        Set<String> set = new HashSet<String>();
 
-		// Using add() method to add elements into the Set
-		set.add("Welcome");
-		set.add("To");
-		set.add("Geeks");
-		set.add("4");
-		set.add("Geeks");
+        // Using add() method to add elements into the Set
+        set.add("Welcome");
+        set.add("To");
+        set.add("Geeks");
+        set.add("4");
+        set.add("Geeks");
 
-		// Displaying the Set
-		System.out.println("Set: " + set);
+        // Displaying the Set
+        System.out.println("Set: " + set);
 
-		// Check for "Geeks" in the set
-		System.out.println("Does the Set contains 'Geeks'? "
-						+ set.contains("Geeks"));
+        // Check for "Geeks" in the set
+        System.out.println("Does the Set contains 'Geeks'? "
+                        + set.contains("Geeks"));
 
-		// Check for "4" in the set
-		System.out.println("Does the Set contains '4'? "
-						+ set.contains("4"));
+        // Check for "4" in the set
+        System.out.println("Does the Set contains '4'? "
+                        + set.contains("4"));
 
-		// Check if the Set contains "No"
-		System.out.println("Does the Set contains 'No'? "
-						+ set.contains("No"));
-	}
+        // Check if the Set contains "No"
+        System.out.println("Does the Set contains 'No'? "
+                        + set.contains("No"));
+    }
 }
 
 // OUTPUT:
