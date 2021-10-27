e1f4d8ad95e747ec440476aa2932085595f82c87
Zhengjian Kang <zk299@nyu.edu>
2021-10-27 00:24:57 -0400

    Create binary_search_template.py

---
+++ python/binary_search/binary_search_template.py
@@ -0,0 +1,24 @@
+# 群主建议模板，和left + 1 < right相比，减少2次left,right最后的额外判断
+
+left, right = 0, sys.maxsize
+
+# template 1
+while left < right: # use left < right
+  mid = left + (right - left) // 2
+  if cond(mid) < k:
+    left = mid + 1
+  else:
+    right = mid
+
+# template 2
+while left < right: # use left < right
+  mid = right - (right - left) // 2
+  if cond(mid) < k:
+    left = mid
+  else:
+    right = mid + 1
+    
+# 当左边是mid + 1的时候，使用template 1
+# 当右边是mid + 1的时候，使用template 2
+    
+  
