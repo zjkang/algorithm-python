2dae0f25db23e1a7cc0aea3a59ac2c477abf2d95
Zhengjian Kang <zk299@nyu.edu>
2021-11-01 21:13:08 -0400

    Update Template_Union_Find.py

--- python/union_find/Template_Union_Find.py
+++ python/union_find/Template_Union_Find.py
@@ -17,3 +17,23 @@ class UF:
             self.father[y] = x
         else:
             self.father[x] = y
+
+            
+# function version
+
+father = [0] * n
+for i in range(n):
+    father[i] = i
+
+def find_father(x):
+    if father[x] != x:
+        father[x] = find_father(father[x])
+    return father[x]
+
+def union(x, y):
+    x = find_father(x)
+    y = find_father(y)
+    if x < y:
+        father[y] = x
+    else:
+        father[x] = y
